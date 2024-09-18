964613adb76c7df4efdf1adbc66fa7dfd5e306d0
Ahsan Ali <ahsanalishah48@gmail.com>
2024-09-18 20:42:31 +0500

    Update urls.py
    erd file add

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import PDFExtractAPIView
 from .views import TaxExemptAPIView
+from .views import ClerkActivityAPIView
 urlpatterns = [
     path('extract/', PDFExtractAPIView.as_view(), name='extract_pdf'),
     path('tax-exempt/', TaxExemptAPIView.as_view(), name='tax_exempt'),
+    path('clerk-activity/', ClerkActivityAPIView.as_view(), name='clerk_activity'),
 ]
