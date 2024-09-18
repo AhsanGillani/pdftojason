8080fee3a2321b9fee5e7d968211993753ddc5f1
Ahsan Ali <ahsanalishah48@gmail.com>
2024-09-18 20:11:41 +0500

    Update urls.py
    add new endpoint for second dcoument

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import PDFExtractAPIView
-
+from .views import TaxExemptAPIView
 urlpatterns = [
     path('extract/', PDFExtractAPIView.as_view(), name='extract_pdf'),
+    path('tax-exempt/', TaxExemptAPIView.as_view(), name='tax_exempt'),
 ]
