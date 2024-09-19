639b7ce92894f94d25ae21260f1c3347a733c310
Ahsan Ali <ahsanalishah48@gmail.com>
2024-09-19 15:27:58 +0500

    Update views.py
    ADD INDEX FUNCTION HERE

--- api/views.py
+++ api/views.py
@@ -6,6 +6,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import status
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
 
 class PDFExtractAPIView(APIView):
     parser_classes = (MultiPartParser, FormParser)
