9c2b568182c3ba81021500541b5a9aa92b48d9f6
Ahsan Ali <ahsanalishah48@gmail.com>
2024-09-19 13:57:06 +0500

    Update urls.py
    4th pdf

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from .views import PDFExtractAPIView
 from .views import TaxExemptAPIView
 from .views import ClerkActivityAPIView
+from .views import FinalAuditAPIView
 urlpatterns = [
     path('extract/', PDFExtractAPIView.as_view(), name='extract_pdf'),
     path('tax-exempt/', TaxExemptAPIView.as_view(), name='tax_exempt'),
     path('clerk-activity/', ClerkActivityAPIView.as_view(), name='clerk_activity'),
+    path('final-audit-extract/', FinalAuditAPIView.as_view(), name='final-audit-extract'),
+
+
 ]
