f6607787e1e7f7458dc0ac778b83fd1d3f5ff29a
Ahsan Ali <ahsanalishah48@gmail.com>
2024-09-27 17:35:56 +0500

    Update urls.py
    5th Video Added

--- api/urls.py
+++ api/urls.py
@@ -3,11 +3,16 @@ from .views import PDFExtractAPIView
 from .views import TaxExemptAPIView
 from .views import ClerkActivityAPIView
 from .views import FinalAuditAPIView
+from .views import OccupancyForecastAPIView
+
+
+
 urlpatterns = [
     path('extract/', PDFExtractAPIView.as_view(), name='extract_pdf'),
     path('tax-exempt/', TaxExemptAPIView.as_view(), name='tax_exempt'),
     path('clerk-activity/', ClerkActivityAPIView.as_view(), name='clerk_activity'),
     path('final-audit-extract/', FinalAuditAPIView.as_view(), name='final-audit-extract'),
+    path('occupancy-forecast/', OccupancyForecastAPIView.as_view(), name='occupancy_forecast'),
 
 
 ]
