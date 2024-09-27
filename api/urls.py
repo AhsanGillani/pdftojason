38f7a1846f252caa2298138bf847c1e8ff488a6f
Ahsan Ali <ahsanalishah48@gmail.com>
2024-09-27 20:02:47 +0500

    Update urls.py
    6th View Addeed

--- api/urls.py
+++ api/urls.py
@@ -4,15 +4,17 @@ from .views import TaxExemptAPIView
 from .views import ClerkActivityAPIView
 from .views import FinalAuditAPIView
 from .views import OccupancyForecastAPIView
-
-
-
+from .views import RateTypeTrackingAPIView
 urlpatterns = [
+
     path('extract/', PDFExtractAPIView.as_view(), name='extract_pdf'),
     path('tax-exempt/', TaxExemptAPIView.as_view(), name='tax_exempt'),
     path('clerk-activity/', ClerkActivityAPIView.as_view(), name='clerk_activity'),
     path('final-audit-extract/', FinalAuditAPIView.as_view(), name='final-audit-extract'),
     path('occupancy-forecast/', OccupancyForecastAPIView.as_view(), name='occupancy_forecast'),
+    path('rate-type-tracking/', RateTypeTrackingAPIView.as_view(), name='rate_type_tracking'),
+    
+    
 
 
 ]
